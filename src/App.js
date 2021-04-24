145905a48e9013fdfa0cf451ba490216afe078e9
Sihyung Lee <touchthesky80s@gmail.com>
2021-04-25 00:02:35 +0900

    06-4

--- src/App.js
+++ src/App.js
@@ -24,23 +24,26 @@ class App extends React.Component {
     render() {
         const { isLoading, movies } = this.state;
         return (
-            <div>
-                {isLoading
-                    ? 'Loading...'
-                    : movies.map((movie) => {
-                          console.log(movie);
-                          return (
-                              <Movie
-                                  key={movie.id}
-                                  id={movie.id}
-                                  year={movie.year}
-                                  title={movie.title}
-                                  summary={movie.summary}
-                                  poster={movie.medium_cover_image}
-                              />
-                          );
-                      })}
-            </div>
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movie">
+                        {movies.map((movie) => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
         );
     }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, title, year, summary, poster }) {
+function Movie({ title, year, summary, poster }) {
     return (
-        <h4>
-            {id}
-            {title}
-            {year}
-            {summary}
-            {poster}
-        </h4>
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
     );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
