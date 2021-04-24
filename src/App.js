89c3dc03aeac4d630fec4146b8ecb156c1bb58c6
Sihyung Lee <touchthesky80s@gmail.com>
2021-04-24 23:42:06 +0900

    06-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
     state = {
@@ -12,15 +13,35 @@ class App extends React.Component {
             data: {
                 data: { movies },
             },
-        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        } = await axios.get(
+            'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+        );
         this.setState({ movies, isLoading: false });
     };
     componentDidMount() {
         this.getMovies();
     }
     render() {
-        const { isLoading } = this.state;
-        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+        const { isLoading, movies } = this.state;
+        return (
+            <div>
+                {isLoading
+                    ? 'Loading...'
+                    : movies.map((movie) => {
+                          console.log(movie);
+                          return (
+                              <Movie
+                                  key={movie.id}
+                                  id={movie.id}
+                                  year={movie.year}
+                                  title={movie.title}
+                                  summary={movie.summary}
+                                  poster={movie.medium_cover_image}
+                              />
+                          );
+                      })}
+            </div>
+        );
     }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie({ id, title, year, summary, poster }) {
+    return (
+        <h4>
+            {id}
+            {title}
+            {year}
+            {summary}
+            {poster}
+        </h4>
+    );
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
